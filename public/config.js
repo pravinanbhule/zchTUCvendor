1c3bb1ecb903589951539698983a9fd89f164adf
pravin.anbhule <pravin.anbhule@delphianlogic.com>
2023-11-06 17:25:07 +0530

    updated to local env

---
+++ public/config.js
@@ -0,0 +1,8 @@
+var App_Config = {
+  //API_Base_URL: "https://wap-tuc-api-prod-001.azurewebsites.net/",
+  //API_Base_URL: "https://wap-tuc-api-uat-001.azurewebsites.net/",
+  //API_Base_URL: "https://wap-uwga-api-dev-001.azurewebsites.net/",
+  //API_Base_URL: "https://dlzchapi.azurewebsites.net/",
+  API_Base_URL: "https://oktadlzchapi.azurewebsites.net/",
+  //API_Base_URL: "http://192.168.0.7/ZurichAPI/",
+};
--- src/oktaauth/Config.js
+++ src/oktaauth/Config.js
@@ -2,10 +2,10 @@ const oktaAuthConfig = {
   // Note: If your app is configured to use the Implicit flow
   // instead of the Authorization Code with Proof of Code Key Exchange (PKCE)
   // you will need to add `pkce: false`
-  //issuer: "https://dev-97242534.okta.com/oauth2/default",
-  //clientId: "0oa5t6jij44rNLQOk5d7",
-  issuer: "https://zurichtest.oktapreview.com/oauth2/aus16yog9z58Gq5LO0h8",
-  clientId: "0oa16yoi5gddu5ZXx0h8",
+  issuer: "https://dev-97242534.okta.com/oauth2/default",
+  clientId: "0oa5t6jij44rNLQOk5d7",
+  //issuer: "https://zurichtest.oktapreview.com/oauth2/aus16yog9z58Gq5LO0h8",
+  //clientId: "0oa16yoi5gddu5ZXx0h8",
   //issuer: "https://zurich.okta-emea.com/oauth2/aus8962grhVCUonuz0i7",
   //clientId: "0oa8960ob0CS6ZizE0i7",
   redirectUri: window.location.origin + "/login/callback",
