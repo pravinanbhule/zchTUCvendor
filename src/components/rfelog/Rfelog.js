c8e1f3da879661e3f4555069d0999b47d4d3331f
Piyush Khichadiya <piyush.khichadiya@contractors.delphianlogic.com>
2024-04-24 10:54:22 +0530

    changes related incountry filter feedback

--- src/components/rfelog/Rfelog.js
+++ src/components/rfelog/Rfelog.js
@@ -2302,39 +2302,45 @@ function Rfelog({ ...props }) {
                   ) : (
                     ""
                   )}
-                  <div className="advance-filter-btn-container mt-5">
-                    <div
-                      className={`advance-filter-btn ${
-                        isInCountryfilterApplied ? "selected" : "normal"
-                      }`}
-                      onClick={handlesetInCountrySearch}
-                    >
-                      Incountry Search
-                    </div>
-                  </div>
-                  {isInCountryfilterApplied ? (
-                    <div className="filter-advance">
-                      <div className="filter-container container">
-                        <div className="row">
-                          {filterdomfields.Incountry.length
-                            ? filterdomfields.Incountry.map((item) =>
-                                item.componenttype === "FrmDatePicker" ? (
-                                  Filterdomobj(item)
-                                ) : (
-                                  <div
-                                    className={`frm-filter col-md-${item.colspan}`}
-                                  >
-                                    {Filterdomobj(item)}
-                                  </div>
-                                )
-                              )
-                            : "Loading..."}
+                  {filterdomfields.common.length > 0 && filterdomfields.Incountry.length === 0 ?
+                    "" 
+                    :
+                    <>
+                      <div className="advance-filter-btn-container mt-5">
+                        <div
+                          className={`advance-filter-btn ${
+                            isInCountryfilterApplied ? "selected" : "normal"
+                          }`}
+                          onClick={handlesetInCountrySearch}
+                        >
+                          Incountry Search
                         </div>
                       </div>
-                    </div>
-                  ) : (
-                    ""
-                  )}
+                      {isInCountryfilterApplied ? (
+                        <div className="filter-advance">
+                          <div className="filter-container container">
+                            <div className="row">
+                              {filterdomfields.Incountry.length
+                                ? filterdomfields.Incountry.map((item) =>
+                                    item.componenttype === "FrmDatePicker" ? (
+                                      Filterdomobj(item)
+                                    ) : (
+                                      <div
+                                        className={`frm-filter col-md-${item.colspan}`}
+                                      >
+                                        {Filterdomobj(item)}
+                                        </div>
+                                    )
+                                  )
+                                : filterdomfields.common.length > 0 && filterdomfields.Incountry.length === 0 ? "" : "Loading..."}
+                            </div>
+                          </div>
+                          </div>
+                      ) : (
+                        ""
+                      )}
+                    </>
+                  }
                   <div className="btn-container">
                     <div
                       className={`btn-blue ${
