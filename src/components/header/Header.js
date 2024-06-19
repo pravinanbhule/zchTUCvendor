65d714b4a3620633f3fd1816f121967f72ab5c74
Piyush Khichadiya <piyush.khichadiya@contractors.delphianlogic.com>
2024-06-19 11:18:02 +0530

    changes realted page responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LoggedInUser from "./Loginuser";
 
 function Header({ ...props }) {
+
+  const [isDesktop, setIsDesktop] = useState(window.matchMedia("(min-width: 769px)").matches)
+
+  useEffect(() => {
+    window
+    .matchMedia("(min-width: 769px)")
+    .addEventListener('change', e => setIsDesktop( e.matches ));
+  }, []);
+
   return (
     <div className="header-container">
       <div className="site-logo"></div>
-      <div className="header-title">Technical Underwriting Connect (TUC)</div>
+      {isDesktop && (
+        <div className="header-title">Technical Underwriting Connect (TUC)</div>
+      )}
       <LoggedInUser />
     </div>
   );
